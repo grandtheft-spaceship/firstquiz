527004a1d4e38097ee51a613c662097965f3a851
grandtheft-spaceship <justdizon71@gmail.com>
2015-12-02 20:21:01 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstquiz::Application.config.secret_key_base = '3a63142bc7842a26be6efbe70a7dd12143f629856c9d6dfa49df1cf2947e9b12065308e4b8414e0523526a804883e571a59c784f0620f3f8af589b7725884128'
